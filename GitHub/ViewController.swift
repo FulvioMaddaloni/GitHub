38f046efd03233a533f3b64bacc4d1a89b4b53cf
Fulvio Maddaloni <fulviomaddaloni@10-20-49-207.0200.fed.it.iosda.org>
2017-12-07 17:10:02 +0100

    Initial Commit

---
+++ GitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub
+//
+//  Created by Fulvio Maddaloni on 07/12/17.
+//  Copyright © 2017 Fulvio Maddaloni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
